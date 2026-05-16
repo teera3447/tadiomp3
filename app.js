cebf2a50b31e5fbb1d8f136a86eb5bb3d61d4d60
teera3447 <steera32@gmail.com>
2026-05-16 11:42:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-let songs = JSON.parse(localStorage.getItem("songs")) || [
+var songs = [
 
 {
-title:"เพลงตัวอย่าง MP3",
+title:"เพลงลูกทุ่ง",
 category:"ลูกทุ่ง",
 url:"media/song1.mp3",
-downloads:0
-},
-
-{
-title:"เพลงตัวอย่าง M4A",
-category:"สตริง",
-url:"media/song2.m4a",
-downloads:0
-},
-
-{
-title:"วิดีโอ 3GP",
-category:"นักร้อง",
-url:"media/video.3gp",
 downloads:0
 }
 
@@ -25,114 +11,21 @@ downloads:0
 
 function renderSongs(){
 
-let search =
-document.getElementById("search")
-.value.toLowerCase();
+var html = "";
 
-let category =
-document.getElementById("category")
-.value;
+for(var i=0;i<songs.length;i++){
 
-let html = "";
-
-songs.forEach((song,index)=>{
-
-if(
-song.title.toLowerCase().includes(search)
-){
-
-if(
-category=="all" ||
-song.category==category
-){
-
-html += `
-<div class="song">
-
-<b>${song.title}</b><br>
-
-หมวด:
-${song.category}<br>
-
-ดาวน์โหลด:
-${song.downloads}<br>
-
-<a class="download"
-href="${song.url}"
-download
-onclick="countDownload(${index})">
-
-ดาวน์โหลด
-
-</a>
-
-</div>
-`;
+html +=
+'<div class="song">' +
+'<b>' + songs[i].title + '</b><br>' +
+'หมวด: ' + songs[i].category + '<br>' +
+'<a href="' + songs[i].url + '">ดาวน์โหลด</a>' +
+'</div>';
 
 }
 
-}
-
-});
-
-document.getElementById("songs")
-.innerHTML = html;
+document.getElementById("songs").innerHTML = html;
 
 }
-
-function countDownload(index){
-
-songs[index].downloads++;
-
-localStorage.setItem(
-"songs",
-JSON.stringify(songs)
-);
-
-}
-
-function uploadSong(){
-
-let title =
-document.getElementById("title").value;
-
-let category =
-document.getElementById("uploadCategory").value;
-
-let file =
-document.getElementById("file").files[0];
-
-if(!file){
-alert("เลือกไฟล์");
-return;
-}
-
-let url = URL.createObjectURL(file);
-
-songs.push({
-
-title:title,
-category:category,
-url:url,
-downloads:0
-
-});
-
-localStorage.setItem(
-"songs",
-JSON.stringify(songs)
-);
-
-renderSongs();
-
-alert("อัปโหลดแล้ว");
-
-}
-
-document.getElementById("search")
-.addEventListener("keyup",renderSongs);
-
-document.getElementById("category")
-.addEventListener("change",renderSongs);
 
 renderSongs();
